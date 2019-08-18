2c61189a4ffc8417ceb617b64f0e097dd44ba038
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-08-18 16:47:17 -0500

    Clase2_update

--- Clase_2/main.lua
+++ Clase_2/main.lua
@@ -1,16 +1,49 @@
 local i=0 
 local imagen=nil
+local music= nil
+local golpe=nil
+
+local ox,oy = 100,300
+local lado = 1
+local w,h
+local r = 0
 	
 function love.load()
 	imagen = love.graphics.newImage("Hamster.png")
+	w,h = imagen:getDimensions( )
+
+	music = love.audio.newSource("hamster.wav","stream" )
+
+
+	music:setLooping(true)
+	music:play()
+
+	golpe = love.audio.newSource("golpe.ogg","static" )
 end
 
 function love.draw()
-	love.graphics.print("hola n°" .. i ,100,100)
+	love.graphics.print("click n°" .. i ,100,100)
 
-	love.graphics.draw(imagen,100,300)
+	love.graphics.draw(imagen,ox,oy,r,lado,1,w/2,h/2)
 end
 
 function love.update(dt)
+	r=r+dt
+end
+
+function love.keypressed(key)
+	if key=="a" then
+		ox=ox-5
+		lado = -1
+	elseif key=="d" then
+		ox=ox+5
+		lado = 1
+	end
+end
+
+function love.mousepressed(x,y,button)
+	ox,oy=x,y
+	golpe:play()
+
 	i=i+1
 end
